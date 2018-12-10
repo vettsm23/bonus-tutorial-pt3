10e122294f05b533f6fbd6e75707a069b143351b
vettsm23 <42977979+vettsm23@users.noreply.github.com>
2018-12-09 23:37:09 -0500

    git for bonus pt 3

---
+++ Assets/Scripts/NavMeshBaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshBaker : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshSurface[] navMeshSurfaces;
+
+    void Start()
+    {
+        for(int i = 0;  i < navMeshSurfaces.Length; i++)
+        {
+            navMeshSurfaces[i].BuildNavMesh();
+        }
+    }
+
+}
